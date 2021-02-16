b0086e91d67f3dead2c825eb279917ea83d0db87
hard <harleydav@mail.ru>
2021-02-16 09:15:23 +0300

    init

---
+++ src/main/java/com/hard/repositories/BandRepository.java
@@ -0,0 +1,15 @@
+package com.hard.repositories;
+
+import com.hard.models.Band;
+
+import java.util.Collection;
+
+public class BandRepository {
+    public Collection<Band> getAll() {
+        return null;
+    }
+
+    public Band getById(long id) {
+        return null;
+    }
+}
---
+++ src/main/java/com/hard/repositories/SongRepository.java
@@ -0,0 +1,15 @@
+package com.hard.repositories;
+
+import com.hard.models.Song;
+
+import java.util.Collection;
+
+public class SongRepository {
+    public Collection<Song> getAll() {
+        return null;
+    }
+
+    public Song getById(long id) {
+        return null;
+    }
+}
