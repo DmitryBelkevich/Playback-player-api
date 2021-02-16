da23e8e2f5d69a3ea7ba2b49918915b3a54ea3ba
hard <harleydav@mail.ru>
2021-02-16 07:58:04 +0300

    init

--- src/main/java/com/hard/models/Song.java
+++ src/main/java/com/hard/models/Song.java
@@ -3,4 +3,13 @@ package com.hard.models;
 import com.hard.models.abstracts.Model;
 
 public class Song extends Model {
+    private String title;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
 }
