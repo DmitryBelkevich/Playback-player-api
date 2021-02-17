cf223d2bdcd1f99b8f4dfd836b77bf52c349abaf
hard <harleydav@mail.ru>
2021-02-17 21:38:23 +0300

    init

--- src/main/resources/sql/02 select/02 select scores.sql
+++ src/main/resources/sql/02 select/02 select scores.sql
@@ -8,9 +8,13 @@ FROM scores;
 SELECT scores.id           AS "score.id",
        scores.title        AS "score.title",
        scores.url          AS "score.url",
+
+       songs.id            AS "song.id",
        songs.title         AS "song.title",
        songs.key_signature AS "song.key_signature",
        songs.text          AS "song.text",
+
+       bands.id            AS "band.id"
        bands.title         AS "band.title"
 FROM scores
          LEFT JOIN songs ON scores.song_id = songs.id
@@ -22,9 +26,13 @@ WHERE songs.id = 20;
 SELECT scores.id           AS "score.id",
        scores.title        AS "score.title",
        scores.url          AS "score.url",
+
+       songs.id            AS "song.id",
        songs.title         AS "song.title",
        songs.key_signature AS "song.key_signature",
        songs.text          AS "song.text",
+
+       bands.id            AS "band.id"
        bands.title         AS "band.title"
 FROM scores
          LEFT JOIN songs ON scores.song_id = songs.id
