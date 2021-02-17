ae4a3eb4a58dfcb54ffa649b9ab24a1566ee7f25
hard <harleydav@mail.ru>
2021-02-17 19:52:04 +0300

    init

---
+++ src/main/java/com/hard/models/Score.java
@@ -0,0 +1,15 @@
+package com.hard.models;
+
+import com.hard.models.abstracts.Model;
+
+public class Score extends Model {
+    private String title;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
