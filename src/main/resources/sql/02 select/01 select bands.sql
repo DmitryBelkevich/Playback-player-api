18be11e4b56232036351bb110f0154290c83e132
hard <harleydav@mail.ru>
2021-02-17 22:05:24 +0300

    init

--- src/main/resources/sql/02 select/01 select bands.sql
+++ src/main/resources/sql/02 select/01 select bands.sql
@@ -9,6 +9,11 @@ SELECT
        songs.id            AS "song.id",
        songs.title         AS "song.title",
        songs.key_signature AS "song.key_signature",
-       songs.text          AS "song.text"
+       songs.text          AS "song.text",
+
+       scores.id           AS "score.id",
+       scores.title        AS "score.title",
+       scores.url          AS "score.url"
 FROM bands
 RIGHT JOIN songs on bands.id = songs.band_id
+RIGHT JOIN scores on songs.id = scores.song_id
