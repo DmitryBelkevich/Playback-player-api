d9809be9d0242336d53739b9c4d50333f440c43b
hard <harleydav@mail.ru>
2021-02-20 22:23:59 +0300

    init

--- src/main/java/com/hard/Main.java
+++ src/main/java/com/hard/Main.java
@@ -1,54 +1,16 @@
 package com.hard;
 
-import com.hard.models.*;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
-import org.hibernate.cfg.Environment;
+import com.hard.models.Song;
+import com.hard.repositories.SongRepository;
 
-import java.util.Properties;
+import java.util.Collection;
 
 public class Main {
+    private static SongRepository songRepository = new SongRepository();
+
     public static void main(String[] args) {
-        Properties properties = new Properties();
+        Collection<Song> songs = songRepository.getAll();
 
-        // Database connection settings
-        properties.setProperty(Environment.DRIVER, "org.postgresql.Driver");
-        properties.setProperty(Environment.URL, "jdbc:postgresql://localhost:5432/playback_player");
-        properties.setProperty(Environment.USER, "postgres");
-        properties.setProperty(Environment.PASS, "1234");
-
-        // SQL dialect
-        properties.setProperty(Environment.DIALECT, "org.hibernate.dialect.PostgreSQL95Dialect");
-
-        // JDBC connection pool (use the built-in)
-        properties.setProperty(Environment.POOL_SIZE, "1");
-
-        // Echo all executed SQL to stdout
-        properties.setProperty(Environment.SHOW_SQL, "true");
-
-        // Enable Hibernate's automatic session context management
-        properties.setProperty(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
-
-        // Drop the existing tables and create new one
-        properties.setProperty(Environment.HBM2DDL_AUTO, "update");
-
-        Configuration configuration = new Configuration()
-                .setProperties(properties)
-                .addAnnotatedClass(User.class)
-                .addAnnotatedClass(Band.class)
-                .addAnnotatedClass(Song.class)
-                .addAnnotatedClass(Score.class)
-                .addAnnotatedClass(Playback.class)
-                .addAnnotatedClass(Metronome.class);
-
-        SessionFactory sessionFactory = configuration
-                .buildSessionFactory();
-
-        Session session = sessionFactory.getCurrentSession();
-
-        session.close();
-
-        sessionFactory.close();
+        System.out.println();
     }
 }
