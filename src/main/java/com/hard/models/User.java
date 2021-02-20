73a24f2dd2aa1269325d352c679d6bd9b1560829
hard <harleydav@mail.ru>
2021-02-20 19:57:31 +0300

    init

--- src/main/java/com/hard/models/User.java
+++ src/main/java/com/hard/models/User.java
@@ -2,6 +2,11 @@ package com.hard.models;
 
 import com.hard.models.abstracts.Model;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
 public class User extends Model {
     private String name;
 
