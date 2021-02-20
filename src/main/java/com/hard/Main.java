e1fa900c9ad4ad36bcdb6131bdeb78092e0fdaff
hard <harleydav@mail.ru>
2021-02-20 19:45:19 +0300

    init

--- src/main/java/com/hard/Main.java
+++ src/main/java/com/hard/Main.java
@@ -1,79 +1,53 @@
 package com.hard;
 
 import com.hard.models.Band;
+import com.hard.models.Score;
 import com.hard.models.Song;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
 
-import java.sql.*;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
 import java.util.Properties;
 
 public class Main {
-    public static void main(String[] args) throws SQLException {
-        Connection connection = connect();
-        Statement statement = connection.createStatement();
-
-        String sql = "SELECT bands.id            AS \"band.id\",\n" +
-                "       bands.title         AS \"band.title\",\n" +
-                "       songs.id            AS \"song.id\",\n" +
-                "       songs.title         AS \"song.title\",\n" +
-                "       songs.key_signature AS \"song.key_signature\",\n" +
-                "       songs.text          AS \"song.text\"\n" +
-                "FROM songs\n" +
-                "         LEFT JOIN bands ON songs.band_id = bands.id";
-
-        ResultSet resultSet = statement.executeQuery(sql);
-
-        Collection<Band> bands = new ArrayList<>();
-        Band band = null;
-        while (resultSet.next()) {
-            if (band == null)
-                band = new Band();
-
-            Song song = new Song();
-            song.setId(resultSet.getLong("song.id"));
-            song.setTitle(resultSet.getString("song.title"));
-            song.setKeySignature(resultSet.getString("song.key_signature"));
-            song.setText(resultSet.getString("song.text"));
-
-            long current_band_id = resultSet.getLong("band.id");
-            if (current_band_id != band.getId()) {
-                band = new Band();
-
-                band.setId(resultSet.getLong("band.id"));
-                band.setTitle(resultSet.getString("band.title"));
-                band.setSongs(new ArrayList<>());
-                band.getSongs().add(song);
-
-                bands.add(band);
-            } else {
-                band.getSongs().add(song);
-            }
-        }
-
-        System.out.println(bands.size());
-    }
-
-    private static Connection connect() {
-        String url = "jdbc:postgresql://localhost:9999/postgres";
+    public static void main(String[] args) {
         Properties properties = new Properties();
-        properties.setProperty("user", "postgres");
-        properties.setProperty("password", "1234");
 
-        try {
-            Class.forName("org.postgresql.Driver");
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
+        // Database connection settings
+        properties.setProperty(Environment.DRIVER, "org.postgresql.Driver");
+        properties.setProperty(Environment.URL, "jdbc:postgresql://localhost:5432/playback_player");
+        properties.setProperty(Environment.USER, "postgres");
+        properties.setProperty(Environment.PASS, "1234");
 
-        Connection connection = null;
-        try {
-            connection = DriverManager.getConnection(url, properties);
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
+        // SQL dialect
+        properties.setProperty(Environment.DIALECT, "org.hibernate.dialect.PostgreSQL95Dialect");
 
-        return connection;
+        // JDBC connection pool (use the built-in)
+        properties.setProperty(Environment.POOL_SIZE, "1");
+
+        // Echo all executed SQL to stdout
+        properties.setProperty(Environment.SHOW_SQL, "true");
+
+        // Enable Hibernate's automatic session context management
+        properties.setProperty(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+
+        // Drop the existing tables and create new one
+        properties.setProperty(Environment.HBM2DDL_AUTO, "update");
+
+        Configuration configuration = new Configuration()
+                .setProperties(properties)
+                .addAnnotatedClass(Band.class)
+                .addAnnotatedClass(Song.class)
+                .addAnnotatedClass(Score.class);
+
+        SessionFactory sessionFactory = configuration
+                .buildSessionFactory();
+
+        Session session = sessionFactory.getCurrentSession();
+
+        session.close();
+
+        sessionFactory.close();
     }
 }
