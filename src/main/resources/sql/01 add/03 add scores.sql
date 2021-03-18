d175fb1d0712831f3035a94c4a031a4033685579
hard <harleydav@mail.ru>
2021-03-18 03:16:59 +0300

    added song

--- src/main/resources/sql/01 add/03 add scores.sql
+++ src/main/resources/sql/01 add/03 add scores.sql
@@ -545,8 +545,8 @@ VALUES
 (DEFAULT, 'Handclap', '12lcBAAEdziqkJcrHfzdNEROrlIe9P2mQ', 122),
 (DEFAULT, 'Electric Organ', '1rMlvrisaMSIPwY4FDPYw1BPRBUqhLRVh', 122),
 -- Сектор Газа - Демобилизация
-(DEFAULT, 'Full Score', '1HFnYIB_ux1St3zdMaDLydVWY2dDonXF7', 123),
-(DEFAULT, 'Voice', '1QieJnb8MJQdcbvUALX9fFG2r7idCsnmj', 123),
+(DEFAULT, 'Full Score', '1_6FRRbAviPk95GWflRnTqm0Lv2nP7N0v', 123),
+(DEFAULT, 'Voice', '1fkiB2gsNoYo4eQp_C9LHam3knQ6LlHCf', 123),
 (DEFAULT, 'Acoustic Guitar, Electric Guitar 1, Electric Guitar 2', '1xCB-sXEZb1_eFzoc5TwKApvf1nj4oowP', 123),
 (DEFAULT, 'Acoustic Guitar', '1UK6EW9XqYzj-wvF4RKvctKHG3OOABNAt', 123),
 (DEFAULT, 'Electric Guitar 1', '1LTKGqgwN0ooZ5Q5GHYT9OPm-C82AfX_p', 123),
