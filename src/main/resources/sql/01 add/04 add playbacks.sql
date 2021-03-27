2c65acb3e33ea48a1b7ec34175ae49202cfeaae0
hard <harleydav@mail.ru>
2021-03-27 10:27:42 +0300

    added song

--- src/main/resources/sql/01 add/04 add playbacks.sql
+++ src/main/resources/sql/01 add/04 add playbacks.sql
@@ -253,6 +253,7 @@ VALUES
 (DEFAULT, '0', '1kQHGP2fELW804Z_kJTprPrUmwbORDMS8', 109),
 -- Михаил Круг - Владимирский централ
 (DEFAULT, 'original', '1HslSFP_52ExO-RQviYAI4RAz_6EpMjrU', 110),
+(DEFAULT, '0', '1iBb-DCYBQoY4V_M26nA3j-ttMp8luCrA', 110),
 -- Михаил Круг - Кольщик
 (DEFAULT, 'original', '1fvlhRCo7p4Kt10cFsgbny-6VcfObirVE', 111),
 (DEFAULT, '0', '1TPTH2RYljlUMr9H8HZ5n9odSlc-V-EWJ', 111),
