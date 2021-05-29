bd64b0436607949c3d1fae2b0e3e284890564804
hard <harleydav@mail.ru>
2021-05-29 19:17:50 +0300

    added song

--- src/main/resources/sql/01 add/04 add playbacks.sql
+++ src/main/resources/sql/01 add/04 add playbacks.sql
@@ -6,6 +6,8 @@ VALUES
 (DEFAULT, 'original', '1zKkmIf6LTAORRgXGtA4aairbtsy2LAbA', 1),
 -- Bon Jovi - It's My Life
 (DEFAULT, 'original', '1ZhV4e8LrelTti44CgBU3J0QcfP72izZ6', 10),
+-- Gary Moore - The Loner
+(DEFAULT, '0', '13bwfpAl_Y_y950VpY6cQKfBUIiUQwQvZ', 25),
 -- Eurythmics - Sweet Dreams (Are Made of This)
 (DEFAULT, 'original', '1WiC6XGzYRXTpMTtVS6qtJ0Iq6MUI9d1q', 20),
 -- Joan Osborne - One of Us
