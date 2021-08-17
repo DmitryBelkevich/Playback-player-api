23ff927acce03331c96be1fbf6246a1ca4351824
hard <harleydav@mail.ru>
2021-08-17 10:14:37 +0300

    added song

--- src/main/resources/sql/01 add/04 add playbacks.sql
+++ src/main/resources/sql/01 add/04 add playbacks.sql
@@ -333,19 +333,19 @@ VALUES
 (DEFAULT, '0', '1E-myP2M7zOkpOgp3mBgY1fA5TZYnHhbJ', 2340),
 -- Сплин - Выхода нет
 (DEFAULT, 'original', '1TFfR8esb__0DnsP8SMc02xvcfOKSGN3p', 2370),
-(DEFAULT, '0', '1agtSI6Guw2pyf0JF4BQ1OKpBAbrpsvPX', 2370),
+(DEFAULT, '0', '16uHJFmGYW5hdIN_ZeEFJd2VFev1M3uKR', 2370),
 -- Сплин - Линия жизни
 (DEFAULT, 'original', '128sIsRKok-cf4jYYwS6CYWa9xUgLcE7D', 2380),
-(DEFAULT, '0', '1i1TE8CQ1qje46JPPSEMqNSncdKc6AoBQ', 2380),
+(DEFAULT, '0', '1yk2B_QrW9Tojm1KSuzC_72srOCr2N39p', 2380),
 -- Сплин - Моё сердце
 (DEFAULT, 'original', '1AjPSkquyEKmU6iaZQGnejPAX1lk1p4pV', 2390),
-(DEFAULT, '0', '1ilBBPuLUa3pWWSdbr5s2C_wVpXH935aT', 2390),
+(DEFAULT, '0', '14F7W4rstztkmQxbyIduOEKSSKNpzLRAF', 2390),
 -- Сплин - Орбит без сахара
 (DEFAULT, 'original', '1W3RKtn2pNLYhXPX4x58ru3jUAZexr16l', 2400),
 (DEFAULT, '0', '1EUMB6U7BOAEQhl5ualx3trWbCPSuQqe4', 2400),
 -- Сплин - Романс
 (DEFAULT, 'original', '1IGAxFyBv9-cwbQgyYWEtUUQTw6MzfOHT', 2410),
-(DEFAULT, '0', '1y03jkJpseyax1_B20hx_IOigTSiWzFd4', 2410),
+(DEFAULT, '0', '1zKuKRQ2UFY5-Oo_5V9zjd_nw5XDfV-5K', 2410),
 -- Танцы Минус - Половинка
 (DEFAULT, 'original', '1_D-H2wiH2j0WosnHXETXsZKkV2-pr_fL', 2430),
 (DEFAULT, '0', '1AIrDfhM2YtBisszdHB4NXn2HKQLpTOaC', 2430),
