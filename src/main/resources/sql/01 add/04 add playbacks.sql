745c0a5000ec7d393490930f473eb7355868c712
hard <harleydav@mail.ru>
2021-12-23 10:27:52 +0300

    added song

--- src/main/resources/sql/01 add/04 add playbacks.sql
+++ src/main/resources/sql/01 add/04 add playbacks.sql
@@ -254,8 +254,8 @@ VALUES
 (DEFAULT, '-2', '1nI773EMyqZYIWOhVS93FQG58Uz3FW7wO', 1750),
 -- Король и Шут - Ели мясо мужики
 (DEFAULT, 'original', '1C1Ne1uam2icG1LvQ1SO0Tq9AiJLlZcSi', 1760),
-(DEFAULT, '0', '12oPyQHvoJBzPIEr6kTEr4IXH77MmY798', 1760),
-(DEFAULT, '-2', '1NvCHfvG9Lq0hn8Zy01JyISrK6-x3kngM', 1760),
+(DEFAULT, '0', '1iSYQTSTq4jb4KyHRY0XNG_xHWRYSN0_Q', 1760),
+(DEFAULT, '-2', '1DcHM7bktc1IFvE5n5R_TFxgjhU0AOcrC', 1760),
 -- Король и Шут - Жаль, нет ружья!
 (DEFAULT, 'original', '1NvzZg9oaFODT-xkG3FXqV92ZCFkrrq85', 1770),
 (DEFAULT, '0', '1WiuXcOgrtxxPs0PdF-FQYAe089MoPWwX', 1770),
