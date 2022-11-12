a7098821d14105c2797f8d4a691dba3f369bdaab
hard <harleydav@mail.ru>
2022-11-12 15:20:08 +0300

    added song

--- src/main/resources/sql/01 add/05 add metronomes.sql
+++ src/main/resources/sql/01 add/05 add metronomes.sql
@@ -275,7 +275,7 @@ VALUES
 (DEFAULT, null, 2, 4, 125, 1815),
 -- Король и Шут - Охотник
 (DEFAULT, 'Intro, Verse, Pre-Chorus, Break', 4, 4, 178, 1820),
-(DEFAULT, 'Chorus, Solo', 2, 4, 178, 1820),
+(DEFAULT, 'Chorus, Solo, Outro', 2, 4, 178, 1820),
 -- Король и Шут - Проклятый старый дом
 (DEFAULT, null, 4, 4, 116, 1830),
 (DEFAULT, 'Break', 2, 4, 116, 1830),
