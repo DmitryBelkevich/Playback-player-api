1a00866c19fce451f529e575486b507056f51fd8
hard <harleydav@mail.ru>
2023-04-10 11:55:41 +0300

    added song

--- src/main/resources/sql/01 add/04 add playbacks.sql
+++ src/main/resources/sql/01 add/04 add playbacks.sql
@@ -11,7 +11,6 @@ VALUES
 -- Metallica - Nothing Else Matters
 (DEFAULT, 'original', '1OjohWhLORpbmPUv80SprjowrqNNb7lV9', 20),
 (DEFAULT, '0', '1oidoVolAFyS-vzBiQgjCF-LEnxk7Jbck', 20),
-(DEFAULT, '-2', '1qRhZuffUQvvOQnOqaWS9fU06EWhlLtOs', 20),
 -- Metallica - The Unforgiven
 (DEFAULT, 'original', '16bQ6xIEHjVgSBjL1Rb8gs53pDBXV93iZ', 30),
 (DEFAULT, '0', '16gWOZ3tC1uUoAUlwXnwxXpYqlMUdEska', 30),
@@ -40,7 +39,6 @@ VALUES
 (DEFAULT, 'original', '14a5-1Vp0Ty4AS-Uf7841QxOcMTkFp6wV', 90),
 (DEFAULT, '+2', '1Zp7zcYdYSGNAMm04Gbchf1Q5SwZWK0Zr', 90),
 (DEFAULT, '0', '1Kuso3EfG5zmOk4qQ0cIvbw2nghTAtbNX', 90),
-(DEFAULT, '0 Bass', '1QMaiQ9gZ6CInzzKdWLQksYzQLC_F3W6I', 90),
 -- Ария - Беспечный ангел
 (DEFAULT, 'original', '1kg1a0A46M7FfLPkdZ3xfPA2aIkU0K0ov', 100),
 (DEFAULT, '+1', '1tijdBkkiRhBvK1nvrAe12K9_xPE5a-wI', 100),
@@ -61,11 +59,6 @@ VALUES
 -- Кипелов - Я свободен!
 (DEFAULT, 'original', '1G2TPOig-GUVAQcNPDs3yOw1JLDvfbHga', 170),
 (DEFAULT, '0', '1Weqv2d_LS2GQ0_3v2dwHQHcCLuON63A7', 170),
-(DEFAULT, '0 bass', '1o_9Eali-TI4FnqzXtEK0f_fXqe1eMp6p', 170),
-(DEFAULT, '-2', '1Yh3g7_P5TBx47al1_7fqj2e7g8nrXb1h', 170),
-(DEFAULT, '-2 bass', '1dL9_-JMT6m-_echMZjaVkHKgeqaKaj8V', 170),
-(DEFAULT, '-5', '13ySIYC70SqN8hCk1YNcryxPxYt7itbVC', 170),
-(DEFAULT, '-5 bass', '1WhmXNnWnKix_B8deyZ398ChpZL-EIDsy', 170),
 -- Легион - Звезда
 (DEFAULT, 'original', '1YHU6z27YpnTycYruo-86cm0U55OMZAFw', 175),
 (DEFAULT, '0', '1xNwHBiK1CwgJAKfnPY-c-FvRHD7DOcLR', 175),
@@ -90,7 +83,6 @@ VALUES
 -- Nirvana - Smells Like Teen Spirit
 (DEFAULT, 'original', '1rHdXtVO1bl8h0q95KHJX79ZfIycTw6NA', 1030),
 (DEFAULT, '0', '1wrqYOLd-c9k-DNV2I_lvAlLn6pLCYr47', 1030),
-(DEFAULT, '-2', '1SVz_Rsdx7nT-F0obRe5fN06gjGZXSFza', 1030),
 -- No Doubt - Don't Speak
 (DEFAULT, 'original', '1SkLKyQNxqENIxSCgmxF-sVV78uZT7jjy', 1035),
 (DEFAULT, '0', '1Q8-vhGoOyjYLf8Z-t-pmolXnld50jTmg', 1035),
@@ -122,6 +114,7 @@ VALUES
 (DEFAULT, '-3', '1i_HWqPtPraL0MEML0rWeHrMq-tyUJFDO', 1125),
 -- BRUTTO - 12 обезьян
 (DEFAULT, 'original', '1VLFscNbxvL7AFgwMMHwef4DLHr2O6E_J', 1130),
+(DEFAULT, '0', '1OngnvjBS4pTH6CjEsuAq2GRSG8nw_BX-', 1130),
 (DEFAULT, '-2', '1shRpIIQeov8Zm-hGe5-5Cge1V7JxpOBM', 1130),
 -- DREZDEN - Эдельвейс
 (DEFAULT, 'original', '1nVr62lWeXnu4O7I_NmbJeVaI8HyKUozV', 1140),
@@ -156,7 +149,6 @@ VALUES
 (DEFAULT, 'original', '1UQW9aBdBCpTnLoVMNxZpqyeRU91jiw0v', 1190),
 (DEFAULT, '0', '12eQqVL-t2KFJiFrYELfv-1qOoVVd3wf4', 1190),
 (DEFAULT, '-2', '1XxyZbCMGg0bo0agfl5u76wZfHyVFTj6s', 1190),
-(DEFAULT, '-5', '1BRAVOKgDgahOAPrbecwTA3JFPpo_J9ox', 1190),
 -- Аквариум - Город золотой
 (DEFAULT, 'original', '1SJOhp6-EYnR0NeEAWTXGnLGecDyS5L0D', 1200),
 (DEFAULT, '0', '1Y1Z8L9_N7Er8ma3xd_AJ3YGiuNle1kY5', 1200),
@@ -167,7 +159,6 @@ VALUES
 -- Би-2 - Варвара
 (DEFAULT, 'original', '1_KPpISEs09R8_qKFfg0IzhgRXvrumdoC', 1240),
 (DEFAULT, '0', '1pSIMituGfmaCuZXIK0CEckSV4wfduVPV', 1240),
-(DEFAULT, '-2', '1L0Ye3jH8pWyQuWM_SxRP2T4YC1cpky3k', 1240),
 -- Би-2 - Компромисс
 (DEFAULT, 'original', '1IgM7YTqyob7Qje_1jfukRep1WTX4reFl', 1250),
 (DEFAULT, '0', '1se8_EklHqygr7FefbW_SELjhXapJzVlh', 1250),
@@ -182,9 +173,7 @@ VALUES
 (DEFAULT, '0', '1PATYxaqLqDXZSxBPDytGd1ALjGqzgnQ1', 1280),
 -- Валентин Стрыкало - Наше лето
 (DEFAULT, 'original', '11EgRxJwhiEixT0Ucsx94ND0ENLpIJOZ9', 1290),
-(DEFAULT, '+3', '1AvK6pwjpO0bKenFXYJvsigBLEtzMBrld', 1290),
 (DEFAULT, '0', '1mDX8paUIhySkfkB2INyzWcb4LklvNVif', 1290),
-(DEFAULT, '-2', '10GM8ziYhCytr7mB8G_agovrndBy_bmzH', 1290),
 -- Гражданская оборона - Вселенская большая любовь
 -- Гражданская оборона - Всё идёт по плану
 (DEFAULT, 'original', '1BRYZ8tN6NKF4oZyFHZ6XlpsiJ-7c0Nke', 1310),
@@ -226,7 +215,6 @@ VALUES
 -- ДДТ - Что такое осень
 (DEFAULT, 'original', '1G-2pWpl6W2YVgW6xpWuAlu5Yq5S1XRkn', 1460),
 (DEFAULT, '0', '1_8G0AShZ7dYktcmTfbtil0mkFUXVNyNN', 1460),
-(DEFAULT, '-2', '1nUBcSynGy45bSQn7gtSNl7IeXvF99NA8', 1460),
 -- ДДТ - Это всё...
 (DEFAULT, 'original', '1d-kbiq9WJv9Z7RbG1muRajg4xrCiegtm', 1470),
 -- Жуки - Батарейка
@@ -236,11 +224,9 @@ VALUES
 -- Звери - До скорой встречи
 (DEFAULT, 'original', '14K1Fdr3fu6ARkmhYHD2FT-ml9jAuxspa', 1530),
 (DEFAULT, '0', '1APMX6R1YINhVY1RoObfgmyTjz_ZNF9zo', 1530),
-(DEFAULT, '-2', '1P7UDiNBADfX7_YW-jrjxct4vv0pkXcf2', 1530),
 -- Звери - Напитки покрепче
 (DEFAULT, 'original', '1sNE--xVQ0bSvgezcp2orq2szpTJ78wcP', 1540),
 (DEFAULT, '0', '1h0R3q6zLeHNkcNuNb_B9FAMFD7K-FiNb', 1540),
-(DEFAULT, '-2', '1xHSxKi7SGSDQyaApZ0vG12nfDQxhOn2a', 1540),
 -- Звери - Просто такая сильная любовь
 (DEFAULT, 'original', '1cetiTwl2v4MGh_vpmLuBVkdC77n76DI-', 1550),
 (DEFAULT, '0', '1CCj72LUVPZWozPXuWJCYU0eRZUJsWyZE', 1550),
@@ -250,7 +236,6 @@ VALUES
 -- Земляне - Трава у дома
 (DEFAULT, 'original', '1ZGDLEl-5bydc6tu1AXs7OfEzSi4lQTrh', 1570),
 (DEFAULT, '0', '1WglpwE3om9EdHmVf_Bi-F9qeptk2JGEJ', 1570),
-(DEFAULT, '-2', '1Di8hWcrqGjcJfHUj_SeOknyBVKd1Wcrr', 1570),
 -- Zемфира - Искала
 (DEFAULT, 'original', '1IwQYmddIakZvif0C5RizE1Zo95-dbd8d', 1581),
 (DEFAULT, '0', '14GihNTNJIx01Wk2IZyw3R0Elg5hbOgfe', 1581),
@@ -336,7 +321,7 @@ VALUES
 -- Король и Шут - Ведьма и осёл
 (DEFAULT, 'original', '1-zoXaoppJ8gfAAdIxhD8PXn6l_mTv5J_', 1723),
 (DEFAULT, '0', '1U0kOJC5DiAKr-A36hInZ4fN3HlJm59Cs', 1723),
-(DEFAULT, '-5', '1zyWGbOKajarXkusY84JOUvxj6UFoZXc1', 1723),
+(DEFAULT, '-3', '1oPyBdBJ23g7UoxE5NCbJgppGAOuQy2Qc', 1723),
 -- Король и Шут - Воспоминание о былой любви
 (DEFAULT, 'original', '1QxAgG3f1KArNxnkEmdGTUQSvKZYv9PwF', 1726),
 (DEFAULT, '0', '1FNEoo8iFXwFZTZusOnLyHZcE0DhwiBch', 1726),
@@ -345,11 +330,9 @@ VALUES
 -- Король и Шут - Два вора и монета
 (DEFAULT, 'original', '1tA3ZwTSUMQ1F9Su5UJ6OnqkVGmq-hPCg', 1730),
 (DEFAULT, '0', '1AdRjz5qq0ObsewodkqEMxIr233lY6SFX', 1730),
-(DEFAULT, '-2', '1IbkiS7sM4zWorde2AtYFx0dR0NzJr8GE', 1730),
 -- Король и Шут - Два монаха в одну ночь
 (DEFAULT, 'original', '1FMAXuvkQIVjnxs7Odx36rkCVMzgFrQar', 1735),
 (DEFAULT, '0', '1q88fQxtAgVz3kilmCZgZnDvJcKw4-Jnr', 1735),
-(DEFAULT, '-2', '1U3baEUmixSURmDZzGpOOgufDDSH4W_Vw', 1735),
 -- Король и Шут - Дурак и молния
 (DEFAULT, 'original', '1a4DgfuiaB-irTmAmn-iW64kgBlaUgseG', 1740),
 (DEFAULT, '0', '1I806AnkGJB6ezB3LdKbi5d8V6PdClULE', 1740),
@@ -379,13 +362,11 @@ VALUES
 (DEFAULT, 'original', '1_a5-6MuaDVE3cDtsgncZL32lQ8jc0Eon', 1810),
 (DEFAULT, '0', '1TCnPcBkF12z8IL90GjBdeXCxBIzFk5V5', 1810),
 (DEFAULT, '-2', '1QcK8FloA5XcWJjovuSnbtvf3jYKO_eTm', 1810),
-(DEFAULT, '0 bass', '1wc1ISyhFbowoNYh_25aS6yOaMDIfpuv5', 1810),
 -- Король и Шут - Наблюдатель
 (DEFAULT, 'original', '1D5v7OHMYq4JDS730g8QCVXg0kDvKnxgc', 1815),
 -- Король и Шут - Охотник
 (DEFAULT, 'original', '1lRpY_XALedsOv253_K7OEALOLR-1zKAM', 1820),
 (DEFAULT, '0', '1qP1H57tJs1WMWPngi7YReJq9S21af-AR', 1820),
-(DEFAULT, '-2', '1teQmNxleqzU99R-7wapME7EZmJhnW-Jc', 1820),
 -- Король и Шут - Проклятый старый дом
 (DEFAULT, 'original', '1ubGf2pFgLUAz7Xv8N7K4kkgv2zjXtQup', 1830),
 (DEFAULT, '0', '18qmcM03QaXLVnAhP1-Mk-PiZaOy1KyjN', 1830),
@@ -407,17 +388,12 @@ VALUES
 -- Ленинград - Терминатор
 (DEFAULT, 'original', '1E_wDM4rT1cEjt0o1ZFxtQM0XnWFVCxJX', 1970),
 (DEFAULT, '0', '1OlHrTMzg1lysRzmBSFhPssCYtkO9OChh', 1970),
-(DEFAULT, '-2', '13K4OhOydrXyG8a4ZAzhEYZ2SVD1AGfge', 1970),
 -- Сергей Шнуров - Привет Морриконе
 (DEFAULT, 'original', '1KEQcC9VQBO1-BIQzrJmtuAmKLU2VDfoF', 1974),
 (DEFAULT, '0', '1pkSjHrqimgdgtidKaxTelSxhdiNVMSld', 1974),
-(DEFAULT, '-2', '1QBEOjgPkIBcpNcaN5oFrJCANGb03kiHP', 1974),
 -- Леприконсы - Хали-Гали
 (DEFAULT, 'original', '1naD3cdTD8EAVTU0_Adup-Mv9j7bNQm0D', 1980),
 (DEFAULT, '0', '1iYs2co518AK064gXh5_A6z3EPsif05jr', 1980),
-(DEFAULT, '0 bass', '140N4KJYwmvWxF0zKm-Sj4pPE0LCQM6T1', 1980),
-(DEFAULT, '-2', '19Xw665DVtsK7g7l_lOnizaM41nDDE3Nm', 1980),
-(DEFAULT, '-2 bass', '19gj--eSxbRCKbATvAKHejfGsRoHc06js', 1980),
 -- Ляпис Трубецкой - Belarus Freedom
 (DEFAULT, 'original', '1kX6ECO39N49fBFBmrlZoOfyT7UeTm2Dx', 1985),
 -- Ляпис Трубецкой - Ау
@@ -430,7 +406,6 @@ VALUES
 -- Ляпис Трубецкой - В платье белом
 (DEFAULT, 'original', '1CTcxTFSN046zNw3gNcF_XgSs9z-F7VWV', 2010),
 (DEFAULT, '0', '1OdwsmaCYxVcGfS-0bmyTnxHtFUGpIs2t', 2010),
-(DEFAULT, '-2', '1cZaKSaokQH0j99HiZARtPuPZ9uvFt7a6', 2010),
 -- Ляпис Трубецкой - Грай
 (DEFAULT, 'original', '1Xycelip9Uav7ZwpJ7o6i4xZhTHSmByvz', 2020),
 (DEFAULT, '0', '1L1Sc81nIx7_RtCMqFymStmFAtIMi3_51', 2020),
@@ -438,11 +413,9 @@ VALUES
 -- Ляпис Трубецкой - Евпатория
 (DEFAULT, 'original', '1iERGZKyPPU-fatuxswXkp2GZFFwagLYR', 2030),
 (DEFAULT, '0', '1V_ilEBCOlpWcvttTsCoZqtfM33M7CD_J', 2030),
-(DEFAULT, '-2', '1aCPv0rkU3vwCuQObFGtXjjCUwDv7k1sy', 2030),
 -- Ляпис Трубецкой - Евпатория (remake)
 (DEFAULT, 'original', '1L1_THLRwLAjcuMC1ImiHDCdGIOVz68FA', 2031),
 (DEFAULT, '0', '1ebDKv8icv7koVohTZYEoKJPFSCzVFxWD', 2031),
-(DEFAULT, '-2', '1UQagsR4NP2So-vHWpuE6X2Qq_6Of-_VG', 2031),
 -- Ляпис Трубецкой - Золотая антилопа
 (DEFAULT, 'original', '13m6F5Lm7szwYFcsTZs59stzeFI4a54hl', 2035),
 (DEFAULT, '0', '1MIdv-PonHmL7EkHAI59b-TnJsmxhchju', 2035),
@@ -467,7 +440,6 @@ VALUES
 -- Ляпис Трубецкой - Шут
 (DEFAULT, 'original', '1wuYpSo4CpSIZ7Vj_fx-3VuzxdZY1G348', 2120),
 (DEFAULT, '0', '17M3a6TU93JrWzGchctGVkk2AEhtFc0RB', 2120),
-(DEFAULT, '-2', '13kbg8FPISkEpxauIzCgSIThdFy6-Temq', 2120),
 -- Ляпис Трубецкой - Юность
 (DEFAULT, 'original', '1M2vKk5gKFbtj1mtBkTv529pkmRftPN7R', 2125),
 (DEFAULT, '0', '1MicZk9tJdN6Bs433skXNZ60g21-iMe7A', 2125),
@@ -477,14 +449,12 @@ VALUES
 -- Ляпис Трубецкой - Я верю
 (DEFAULT, 'original', '1YAQLe1erw3zsXEzsFBgkXj6PcJnTfiNa', 2140),
 (DEFAULT, '0', '1SFRBwzL1MqDhRnpLaPZWUl7AQp4sNy9v', 2140),
-(DEFAULT, '-2', '1-V7VkcAkauujXLWjG46b5OWBO2b97J5F', 2140),
 -- Мумий Тролль - Владивосток 2000
 (DEFAULT, 'original', '1I4a-3nldUW83gZPUHOmS3YuktzS9BgQ3', 2150),
 (DEFAULT, '0', '1AOIxXa6tYn3PWTd5DGFWfKjzP_4nCQoU', 2150),
 -- Мумий Тролль - Медведица
 (DEFAULT, 'original', '1YnMY6KxWAKzsFCcWY3T1mDJzy7biZKCz', 2160),
 (DEFAULT, '0', '1HLCuEhJWDRaYlW8go1Xil38lXCol1RYb', 2160),
-(DEFAULT, '-2', '1f-_hrhvaXVL8fq_2aDST3Rhfz6fSo6jC', 2160),
 -- Мумий Тролль - Невеста
 (DEFAULT, 'original', '1Q9Gd67hBG93UOoAYr4X8QwtjGn_QzzsU', 2165),
 (DEFAULT, '0', '1XEia8hegfuZnIEOkbKONvh5nZ8l4M3hg', 2165),
@@ -521,7 +491,6 @@ VALUES
 -- Нервы - Самый дорогой человек
 (DEFAULT, 'original', '1HXbo4bCS4W5Jkg34Zd4FHOq6r2PH_8Ve', 2257),
 (DEFAULT, '0', '1cLlHjLlaguRUgQ14_eRlJMaW24FOK5uq', 2257),
-(DEFAULT, '-2', '1HTatcrpsLV4IpDGsJL00IP4BAsi7k6oW', 2257),
 (DEFAULT, '-4', '1UNU9hKlbOuQWiv4dln3QS_NrWpeqB_2y', 2257),
 -- Нервы - Слишком влюблен
 (DEFAULT, 'original', '1N_4RHh8c-Q-OLJZajHIhc_JQJEyEO5be', 2259),
@@ -541,7 +510,6 @@ VALUES
 (DEFAULT, 'original', '1OC1lsnQEsc3wciMz1L13pZc_EXpsNAHy', 2290),
 (DEFAULT, '+5', '15ecowhpUXfY4fqPlR05bTOHwiQokUJY7', 2290),
 (DEFAULT, '0', '1Eglv2V9Fezm8adFR7JAhgLsXjvvbdS4O', 2290),
-(DEFAULT, '-2', '1Xprl5_rkZyPc5ZGTYP0ms7xC4Ad85eYS', 2290),
 -- Порнофильмы - Я так соскучился
 (DEFAULT, 'original', '1Zw0YoRcOeNcRgs3pP5F_ZBucy-HTFSiY', 2295),
 (DEFAULT, '+5', '10sLOIXNsYU50NJYY3343HVD3Aazm6hTM', 2295),
@@ -550,28 +518,21 @@ VALUES
 -- Сектор Газа - 30 лет
 (DEFAULT, 'original', '1wIDn6ec4OlJ_D1er16Qf2Sk_lPLGsOdb', 2300),
 (DEFAULT, '0', '1jHSNKYNF_TbBwGtYDNpwUbWhuqwtmn94', 2300),
-(DEFAULT, '-1', '1Q3Wt0wmsiNO0ICcIKAa9SK3pQMEe4rCb', 2300),
-(DEFAULT, '-2', '1EV6mNddZ2nZeJnZEhzmPtIURjPDDIEnt', 2300),
 -- Сектор Газа - Бомж
 (DEFAULT, 'original', '1J28XD4xG2X2MHzbJw85S8dOENWfYJEnA', 2305),
 (DEFAULT, '0', '1fLF20wG1pldYg5H0s_RCt_QqqrrD6aO-', 2305),
 -- Сектор Газа - Демобилизация
 (DEFAULT, 'original', '1AOgo6z5PacmGOR_cEVkHImkrNHTP6uUf', 2310),
 (DEFAULT, '0', '1ClW4nvRODv7ved3D_RCfFkAphclT9z3l', 2310),
-(DEFAULT, '-2', '14bCHWbBhGuWc_Ei93qaCt71UILw_lOqL', 2310),
 -- Сектор Газа - Лирика
 (DEFAULT, 'original', '1iOMjlNjamlp1ykLzpyAsMyKc_J1UClC8', 2315),
 (DEFAULT, '0', '1cHTkmOyK8J465YSLrkW17DrB-M9TiYQy', 2315),
-(DEFAULT, '-1', '1TKkDi3MdToOwIDbERVbUF7MQ2iNgsqR1', 2315),
-(DEFAULT, '-2', '1YyDsTxcXiKYlqZ6EJFguOZ_LxQ9vCsg0', 2315),
 -- Сектор Газа - Пора домой
 (DEFAULT, 'original', '1HzP8J_N_ZH0AdHs3PQ3ISUcoPX_EpiNm', 2320),
 (DEFAULT, '0', '1_VMTXtRonuLZI6tZXStcMuZHFbRX8bfS', 2320),
-(DEFAULT, '-2', '1d_qtnbNBG245KxINNFpkvHGb87w8KNSi', 2320),
 -- Сектор Газа - Туман
 (DEFAULT, 'original', '1KmiuMyrSRQa7sg3SMAsKww4OcvavGBuX', 2325),
 (DEFAULT, '0', '1bdpyuCKN9v7NA1qb0pUL9y6o0uwJRHlX', 2325),
-(DEFAULT, '-2', '1NImzIcFMkKKZx6YtInCaGb2cbbYnrlFy', 2325),
 -- Сергей Тихановcкий & Kosmas - Разбуры турмы муры
 (DEFAULT, 'original', '1VEyAlIKFCcQ9jVc4cvkhuPHRXpPNygri', 2330),
 (DEFAULT, '0', '1E-myP2M7zOkpOgp3mBgY1fA5TZYnHhbJ', 2330),
@@ -613,18 +574,15 @@ VALUES
 -- Gary Moore - The Loner
 (DEFAULT, 'original', '1sa7MrV2q_lFLrM4fr7D4s2hOEniZLevZ', 3020),
 (DEFAULT, '0', '151ay9ovdknImnk0abkSHsUTeZgPGaQcY', 3020),
-(DEFAULT, '-2', '1sBewSCld-VR0houptU3zBfCI4dGKElyW', 3020),
 -- Gary Moore - The Prophet
 (DEFAULT, 'original', '1oh8XmPfYiAnqwhV8Q7BLE5eE9RBDx7qo', 3030),
 (DEFAULT, '0', '1wmvBiLTTcA9OK4yKj15AwwqAm9tL5BLc', 3030),
-(DEFAULT, '-2', '1CifscDF6IQ1Etk4iJ41V0C_78MwwP2Q7', 3030),
 
 -- Surf
 
 -- Dick Dale, The Del Tones - Misirlou
 (DEFAULT, 'original', '1E7CAZfXzHg_SjH4udswCuNJtpG_DjupX', 4000),
 (DEFAULT, '0', '14KgwhYjFV-JuFpsWs1kawL517gvVRX1J', 4000),
-(DEFAULT, '-2', '14XXXca9W2wWK0C8hWDMmfGK_Z9BII-5O', 4000),
 
 -- Pop, Dance
 
@@ -648,13 +606,11 @@ VALUES
 -- Ласковый май - Белые розы
 (DEFAULT, 'original', '1lk1jzI-Eiak_956et5pis2WlaUEYj89Z', 5350),
 (DEFAULT, '0', '1k_8wtY1NChX1HL9wlpzZGsArn79gq885', 5350),
-(DEFAULT, '-2', '1PoP2DO5x04twqZcf8aw9ygFDyBuYhTDs', 5350),
 -- Ласковый май - Детство
 (DEFAULT, 'original', '141Sw4RqTHo0DIqVaGpLbc9sqbMsNM0SO', 5400),
 -- Ласковый май - Розовый вечер
 (DEFAULT, 'original', '1guuLUic-2XB1pfeqe0ChCiN7qD-A0yP5', 5450),
 (DEFAULT, '0', '1MpjJHnykectWRCYkWRlzBsaWPHmQpJjE', 5450),
-(DEFAULT, '-2', '1EN6eJpcHijmOt5XdA6r9vK4pdFE6vh2C', 5450),
 -- Ласковый май - Седая ночь
 (DEFAULT, 'original', '1EgjhenWF2bOTP3O1ckKplEfluFIyjl-d', 5500),
 (DEFAULT, '0', '1JWVHP_d7_7iN63LLtXEwvNYpeGlrP3jB', 5500),
@@ -713,7 +669,8 @@ VALUES
 (DEFAULT, 'original', '1e9FkKo4ws0-OwjaUZhEZZNvb1cepK6Um', 8030),
 -- Михаил Боярский - Зеленоглазое такси
 (DEFAULT, 'original', '1TJEQsSf_fhbmfCv9nc82Sxh0P9zSqNmB', 8050),
-(DEFAULT, '-1', '1P2g8Rz3ROpSKocqtpaF0Ubhq8F-iaSxJ', 8050),
+(DEFAULT, '1', '1FscPQT7nhC3M4476bFqncKRQjRYeNcdn', 8050),
+(DEFAULT, '0', '1_urxOxEtukE8n0jtrA16oT7GO20Pjm2h', 8050),
 -- Муслим Магомаев - Серенада Трубадура
 (DEFAULT, 'original', '1JY39qD56ZB8F6jfXFPlP8nLf-oUFCFfx', 8065),
 (DEFAULT, '0', '1tUjPBc-GRjjb_i-ZLINr_RPrkhkiQgdM', 8065),
@@ -734,7 +691,6 @@ VALUES
 (DEFAULT, 'original', '1fvlhRCo7p4Kt10cFsgbny-6VcfObirVE', 9020),
 (DEFAULT, '0', '1_Erncgsx3CemS5iFtXqGy-oSUGTbGwgl', 9020),
 (DEFAULT, '-1', '1IWNUa370D10EL9dbGn8Xb4PaLKOI-0WG', 9020),
-(DEFAULT, '-2', '1KIcxT0-NOYZ44y7FLx9ZnQfrtvgj2IwE', 9020),
 -- Стас Михайлов - Всё для тебя
 (DEFAULT, 'original', '1uvuqFZgbi4Udv7OxQBV80ZVFLBfq854T', 9040),
 (DEFAULT, '0', '1nXI_KD5SNsbFm09njgkp777aQvoLWNxl', 9040),
@@ -745,14 +701,12 @@ VALUES
 -- Ennio Morricone - Chi Mai
 (DEFAULT, 'original', '1ljTRgWJJrmJ2RkX0a49sa0XtyGNohFxR', 10050),
 (DEFAULT, '0', '1LflDZc7ahaxbeoc8wIQMPD4ErUDeQ9QE', 10050),
-(DEFAULT, '-2', '14xqGZagA2UCkvmKucqECtUqZl560-LhY', 10050),
 -- Ennio Morricone - Le Vent, Le Cri
 (DEFAULT, 'original', '1vOtGaOjk6XQcEPQAxjapIJ_Ipw7BgCtn', 10060),
 (DEFAULT, '0', '136PO15pQEFoWUv0YwhjAg6tnYk4Ie6Li', 10060),
 -- Gheorghe Zamfir - The Lonely Shepherd
 (DEFAULT, 'original', '1gYjHBAI3Cc928l_O6rka5fICyYsMv65t', 10100),
 (DEFAULT, '0', '11ep2KnZaO13RqcxwFH4mfg8HvbZNfy1r', 10100),
-(DEFAULT, '-2', '12sahEmgqBk1rnxNS2cXaJUunUBJ77U7S', 10100),
 -- Евгений Дога - Вальс
 (DEFAULT, 'original', '1eZeyLrTeMb4wzE11JS2m-NLTjVpNz47S', 10110),
 (DEFAULT, '0', '143HQyqxE_CHm9YYfllmJDbXGopGE1aDi', 10110),
