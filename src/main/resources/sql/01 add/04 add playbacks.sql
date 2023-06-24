9e5322612ec47cb69b00db028e6de8fccbbf7adf
hard <harleydav@mail.ru>
2023-06-24 14:33:16 +0300

    added song

--- src/main/resources/sql/01 add/04 add playbacks.sql
+++ src/main/resources/sql/01 add/04 add playbacks.sql
@@ -396,6 +396,7 @@ VALUES
 (DEFAULT, '-2', '1ck1vE7Ia3yk61ug4msrYj3P4xMsQPyqx', 1835),
 -- Король и Шут - Танец злобного гения
 (DEFAULT, 'original', '1rNLY9uGD5zebru-bivTvaGkX3SfOZPPo', 1848),
+(DEFAULT, '0', '1KxdzLrOtyiw77_Q_bwMO4BlXK9KGpfuv', 1848),
 -- Ленинград - WWW
 (DEFAULT, 'original', '1j8wixu2so7DW5qIKPGM8WYDfhYVvlEM5', 1950),
 (DEFAULT, '0', '1xTJcxGL6WES6I3WihXlWlG1SKyFVk8Dq', 1950),
